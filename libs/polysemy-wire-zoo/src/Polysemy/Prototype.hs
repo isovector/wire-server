1c1535e57dacc66405af85251dd9ace5fcec82e1
Sandy Maguire <sandy@sandymaguire.me>
2022-10-11 15:31:14 -0700

    chore: format

--- libs/polysemy-wire-zoo/src/Polysemy/Prototype.hs
+++ libs/polysemy-wire-zoo/src/Polysemy/Prototype.hs
@@ -1,52 +1,48 @@
-{-# LANGUAGE AllowAmbiguousTypes         #-}
-{-# LANGUAGE TemplateHaskell             #-}
-
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
 
 module Polysemy.Prototype where
 
-import Polysemy
-import Polysemy.State
-import Imports hiding (read)
 import Control.Arrow (first, second)
+import Imports hiding (read)
+import Polysemy
+import Polysemy.Internal
 import Polysemy.Internal.CustomErrors (FirstOrder)
 import Polysemy.Internal.Tactics (liftT, runTactics)
-import Polysemy.Internal
 import Polysemy.Internal.Union
-
+import Polysemy.State
 
 data Tty m a where
   Read :: Tty m (Maybe String)
   Write :: String -> Tty m ()
 
-
 makeSem ''Tty
 
-
 runTty :: [String] -> Sem (Tty ': r) a -> Sem r (([String], [String]), a)
-runTty ins a
-  = fmap (first $ second reverse)
-  $ runState (ins, [] :: [String])
-  $ reinterpret (\case
-      Read -> do
-        r <- gets $ listToMaybe . fst
-        modify $ first $ drop 1
-        pure r
-      Write s -> do
-        modify $ second (s :)
-                )
-    a
+runTty ins a =
+  fmap (first $ second reverse) $
+    runState (ins, [] :: [String]) $
+      reinterpret
+        ( \case
+            Read -> do
+              r <- gets $ listToMaybe . fst
+              modify $ first $ drop 1
+              pure r
+            Write s -> do
+              modify $ second (s :)
+        )
+        a
 
-replace
-    :: ( Member e r
-       , FirstOrder e "replace"
-       )
-    => (forall x rInitial. e (Sem rInitial) x -> Sem r (Replace x))
-    -> Sem r a
-    -> Sem r a
+replace ::
+  ( Member e r,
+    FirstOrder e "replace"
+  ) =>
+  (forall x rInitial. e (Sem rInitial) x -> Sem r (Replace x)) ->
+  Sem r a ->
+  Sem r a
 replace f = replaceH $ \(e :: e (Sem rInitial) x) ->
   liftT @(Sem rInitial) $ f e
-
 
 data Replace a
   = Replace a
@@ -57,8 +53,8 @@ unreplace :: Replace a -> a
 unreplace (Replace a) = a
 unreplace (AlsoRunReplaced a) = a
 
-
 ------------------------------------------------------------------------------
+
 -- | 'replace \@e' intercepts actions in @e@ and runs the given handler on
 -- them, effectively replacing one handler with another. For example, given the
 -- following:
@@ -99,40 +95,39 @@ unreplace (AlsoRunReplaced a) = a
 -- The replacing handler for @e@ may still call actions in @e@, which will be
 -- forwarded to the replaced handler. This is a useful escape hatch if the
 -- @Replace@ type doesn't give you all the control you'd like.
-replaceH
-    :: Member e r
-    => (forall x r0. e (Sem r0) x -> Tactical e (Sem r0) r (Replace x))
-    -> Sem r a
-    -> Sem r a
+replaceH ::
+  Member e r =>
+  (forall x r0. e (Sem r0) x -> Tactical e (Sem r0) r (Replace x)) ->
+  Sem r a ->
+  Sem r a
 replaceH f (Sem m) = Sem $ \k -> m $ \u ->
   case prj u of
     Just (Weaving e s d y v) ->
       fmap y $ do
-        z <- usingSem k
-          $ runTactics s (raise . d) v (replaceH f . d)
-          $ f e
+        z <-
+          usingSem k $
+            runTactics s (raise . d) v (replaceH f . d) $
+              f e
         for_ (v z) $ \case
-           Replace _ -> pure ()
-           AlsoRunReplaced _ -> void $ k u
+          Replace _ -> pure ()
+          AlsoRunReplaced _ -> void $ k u
         pure $ fmap unreplace z
     Nothing -> k $ hoist (replaceH f) u
 
-
 tryInIO :: Member (Embed IO) r => Tty (Sem r0) x -> Sem r (Replace x)
-tryInIO = fmap Replace . \case
-  Read -> embed $ fmap Just $ fmap ("IO: " ++) getLine
-  Write s -> embed $ putStrLn $ "(IO) " ++ s
-
+tryInIO = \case
+  Read -> fmap Replace . embed $ fmap Just $ fmap ("IO: " ++) getLine
+  Write s -> fmap AlsoRunReplaced . embed $ putStrLn $ "(IO) " ++ s
 
 main :: IO ()
-main = (print =<<) $ runM $ runTty ["hello", "world"] $ replace tryInIO $ do
-  read >>= \case
-    Nothing -> do
-      write "NO INPUT"
-      pure "bad"
-    Just s -> do
-      write s
-      pure @_ @String "ok"
-
-
-
+main = (print =<<) $
+  runM $
+    runTty ["hello", "world"] $
+      replace tryInIO $ do
+        read >>= \case
+          Nothing -> do
+            write "NO INPUT"
+            pure "bad"
+          Just s -> do
+            write s
+            pure @_ @String "ok"
