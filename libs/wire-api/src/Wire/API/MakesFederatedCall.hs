8ed17dfd7ec2041b01a6792b38b81c344416025c
Sandy Maguire <sandy@sandymaguire.me>
2023-01-30 14:36:25 -0800

    feat: generalize CallsFed

--- libs/wire-api/src/Wire/API/MakesFederatedCall.hs
+++ libs/wire-api/src/Wire/API/MakesFederatedCall.hs
@@ -15,13 +15,15 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 
 module Wire.API.MakesFederatedCall
   ( CallsFed,
+    CallsAPI,
     MakesFederatedCall,
-    Component (..),
-    callsFed,
-    unsafeCallsFed,
+    FederatedComponent (..),
+    makesCall,
+    unsafeMakesCall,
   )
 where
 
@@ -41,12 +43,19 @@ import Test.QuickCheck (Arbitrary)
 import Unsafe.Coerce (unsafeCoerce)
 import Wire.Arbitrary (GenericUniform (..))
 
-data Component
+data LocalComponent
+  = InternalBrig
+  deriving (Show, Eq, Generic)
+  deriving (Arbitrary) via (GenericUniform LocalComponent)
+
+data FederatedComponent
   = Brig
   | Galley
   | Cargohold
   deriving (Show, Eq, Generic)
-  deriving (Arbitrary) via (GenericUniform Component)
+  deriving (Arbitrary) via (GenericUniform FederatedComponent)
+
+class MakesCall loc (comp :: loc) (name :: Symbol)
 
 -- | A typeclass corresponding to calls to federated services. This class has
 -- no methods, and exists only to automatically propagate information up to
@@ -55,7 +64,9 @@ data Component
 -- The only way to discharge this constraint is via 'callsFed', which should be
 -- invoked for each federated call when connecting handlers to the server
 -- definition.
-class CallsFed (comp :: Component) (name :: Symbol)
+type CallsFed = MakesCall FederatedComponent
+type CallsAPI = MakesCall LocalComponent
+
 
 -- | A typeclass with the same layout as 'CallsFed', which exists only so we
 -- can discharge 'CallsFeds' constraints by unsafely coercing this one.
@@ -64,22 +75,24 @@ class Nullary
 instance Nullary
 
 -- | Construct a dictionary for 'CallsFed'.
-synthesizeCallsFed :: forall (comp :: Component) (name :: Symbol). Dict (CallsFed comp name)
-synthesizeCallsFed = unsafeCoerce $ Dict @Nullary
+synthesizeMakesCall :: forall loc (comp :: loc) (name :: Symbol). Dict (MakesCall loc comp name)
+synthesizeMakesCall = unsafeCoerce $ Dict @Nullary
 
 -- | Servant combinator for tracking calls to federated calls. Annotating API
 -- endpoints with 'MakesFederatedCall' is the only way to eliminate 'CallsFed'
 -- constraints on handlers.
-data MakesFederatedCall (comp :: Component) (name :: Symbol)
+data MakesAPICall loc (comp :: loc) (name :: Symbol)
 
-instance (HasServer api ctx) => HasServer (MakesFederatedCall comp name :> api :: *) ctx where
+type MakesFederatedCall = MakesAPICall FederatedComponent
+
+instance (HasServer api ctx) => HasServer (MakesAPICall loc comp name :> api :: *) ctx where
   -- \| This should have type @CallsFed comp name => ServerT api m@, but GHC
   -- complains loudly thinking this is a polytype. We need to introduce the
   -- 'CallsFed' constraint so that we can eliminate it via
   -- 'synthesizeCallsFed', which otherwise is too-high rank for GHC to notice
   -- we've solved our constraint.
-  type ServerT (MakesFederatedCall comp name :> api) m = Dict (CallsFed comp name) -> ServerT api m
-  route _ ctx f = route (Proxy @api) ctx $ fmap ($ synthesizeCallsFed @comp @name) f
+  type ServerT (MakesAPICall loc comp name :> api) m = Dict (MakesCall loc comp name) -> ServerT api m
+  route _ ctx f = route (Proxy @api) ctx $ fmap ($ synthesizeMakesCall @loc @comp @name) f
   hoistServerWithContext _ ctx f s = hoistServerWithContext (Proxy @api) ctx f . s
 
 instance HasLink api => HasLink (MakesFederatedCall comp name :> api :: *) where
@@ -90,22 +103,32 @@ instance RoutesToPaths api => RoutesToPaths (MakesFederatedCall comp name :> api
   getRoutes = getRoutes @api
 
 -- | Get a symbol representation of our component.
-type family ShowComponent (x :: Component) :: Symbol where
-  ShowComponent 'Brig = "brig"
-  ShowComponent 'Galley = "galley"
-  ShowComponent 'Cargohold = "cargohold"
+type family HeaderForLocation loc where
+  HeaderForLocation FederatedComponent = "wire-makes-federated-call-to"
+  HeaderForLocation LocalComponent = "wire-makes-api-call-to"
+
+type ShowComponent :: forall loc -> loc -> Symbol
+type family ShowComponent loc x where
+  ShowComponent FederatedComponent 'Brig = "brig"
+  ShowComponent FederatedComponent 'Galley = "galley"
+  ShowComponent FederatedComponent 'Cargohold = "cargohold"
+  ShowComponent LocalComponent 'InternalBrig = "brig"
 
 -- | 'MakesFederatedCall' annotates the swagger documentation with an extension
 -- tag @x-wire-makes-federated-calls-to@.
-instance (HasSwagger api, KnownSymbol name, KnownSymbol (ShowComponent comp)) => HasSwagger (MakesFederatedCall comp name :> api :: *) where
+instance (HasSwagger api,
+          KnownSymbol name,
+          KnownSymbol (HeaderForLocation loc),
+          KnownSymbol (ShowComponent loc comp)
+         ) => HasSwagger (MakesAPICall loc comp name :> api :: *) where
   toSwagger _ =
     toSwagger (Proxy @api)
       & addExtensions
         mergeJSONArray
-        [ ( "wire-makes-federated-call-to",
+        [ ( T.pack $ symbolVal $ Proxy @(HeaderForLocation loc),
             Array
               [ Array
-                  [ String $ T.pack $ symbolVal $ Proxy @(ShowComponent comp),
+                  [ String $ T.pack $ symbolVal $ Proxy @(ShowComponent loc comp),
                     String $ T.pack $ symbolVal $ Proxy @name
                   ]
               ]
@@ -116,28 +139,28 @@ mergeJSONArray :: Value -> Value -> Value
 mergeJSONArray (Array x) (Array y) = Array $ x <> y
 mergeJSONArray _ _ = error "impossible! bug in construction of federated calls JSON"
 
-instance HasClient m api => HasClient m (MakesFederatedCall comp name :> api :: *) where
-  type Client m (MakesFederatedCall comp name :> api) = Client m api
+instance HasClient m api => HasClient m (MakesAPICall loc comp name :> api :: *) where
+  type Client m (MakesAPICalt l loc comp name :> api) = Client m api
   clientWithRoute p _ = clientWithRoute p $ Proxy @api
   hoistClientMonad p _ f c = hoistClientMonad p (Proxy @api) f c
 
 -- | Type class to automatically lift a function of the form @(c1, c2, ...) =>
 -- r@ into @Dict c1 -> Dict c2 -> ... -> r@.
-class SolveCallsFed c r a where
+class SolveMakesCall c r a where
   -- | Safely discharge a 'CallsFed' constraint. Intended to be used when
   -- connecting your handler to the server router.
-  callsFed :: (c => r) -> a
+  makesCall :: (c => r) -> a
 
-instance (c ~ ((k, d) :: Constraint), SolveCallsFed d r a) => SolveCallsFed c r (Dict k -> a) where
-  callsFed f Dict = callsFed @d @r @a f
+instance (c ~ ((k, d) :: Constraint), SolveMakesCall d r a) => SolveMakesCall c r (Dict k -> a) where
+  makesCall f Dict = makesCall @d @r @a f
 
-instance {-# OVERLAPPABLE #-} (c ~ (() :: Constraint), r ~ a) => SolveCallsFed c r a where
-  callsFed f = f
+instance {-# OVERLAPPABLE #-} (c ~ (() :: Constraint), r ~ a) => SolveMakesCall c r a where
+  makesCall f = f
 
 -- | Unsafely discharge a 'CallsFed' constraint. Necessary for interacting with
 -- wai-routes.
 --
 -- This is unsafe in the sense that it will drop the 'CallsFed' constraint, and
 -- thus might mean a federated call gets forgotten in the documentation.
-unsafeCallsFed :: forall (comp :: Component) (name :: Symbol) r. (CallsFed comp name => r) -> r
-unsafeCallsFed f = withDict (synthesizeCallsFed @comp @name) f
+unsafeMakesCall :: forall loc (comp :: loc) (name :: Symbol) r. (MakesCall loc comp name => r) -> r
+unsafeMakesCall f = withDict (synthesizeMakesCall @loc @comp @name) f
